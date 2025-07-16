c4a22f46368d0fe65d56df4754d8a229abb352b7
Jeremy Marube <jeremymarube1@gmail.com>
2025-07-16 21:37:47 +0300

    Adding content

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,21 @@
-// Your code here
+function isPalindrome(word) {
+  if (typeof word !== 'string') {
+    throw new Error("Input must be a string");
+  }
+
+  if (word.length === 0) {
+    return false;
+  }
+
+  if (!/^[A-Za-z]+$/.test(word)) {
+    throw new Error("Input must contain only alphabetic characters");
+  }
+
+  const cleaned = word.toLowerCase();
+  const reversed = cleaned.split('').reverse().join('');
+  return cleaned === reversed;
+}
+
+module.exports = { isPalindrome };
+
+
